7cb8626732910aa8d9b2405e6f3f588cbf6c9365
apschhokar <apschhokar@gmail.com>
2016-06-22 04:43:07 -0400

    Changes made to UI and New Groups class added

---
+++ Groups.swift
@@ -0,0 +1,18 @@
+//
+//  Groups.swift
+//  CelebritySentiment
+//
+//  Created by ajay singh on 6/22/16.
+//  Copyright © 2016 Ajay. All rights reserved.
+//
+
+import Foundation
+
+class groups : NSObject{
+
+    var twitterName:String?
+    var groupName:String?
+    var numberOfCelebrities:Int = 0
+
+
+}
