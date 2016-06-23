18e7aef3b24af28336821372fafbed6f4759b5d3
apschhokar <apschhokar@gmail.com>
2016-06-23 02:38:17 -0400

    Added GroupsViewController and custom table view cell for custom cell

---
+++ CelebritySentiment/GroupsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GroupsTableViewController.swift
+//  CelebritySentiment
+//
+//  Created by ajay singh on 6/23/16.
+//  Copyright © 2016 Ajay. All rights reserved.
+//
+
+import UIKit
+
+class GroupsTableViewController: UITableViewController {
+
+}
